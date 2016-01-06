1c22ebbaa2d5d72d46a095661a56afde7e11016a
Thomas Scheel <thomas.scheel@cfturbo.de>
2016-01-06 18:18:08 +0100

    Cleanup version footer

--- version.php
+++ version.php
@@ -2,8 +2,8 @@
 
 $version_id = "1.9.2";
 
-echo("<hr/><span style=\"text-align:left;\"><a style=\"font-style: italic;\" href='http://freshmeat.net/projects/phplicensewatcher/'>PhpLicensewatcher</a> v. $version_id</span>\n");
-echo("<p><span style=\"text-align:right;\">Page last refreshed at: " . DATE("Y-m-d H:i:s") . "</span>\n");
+echo("<hr/><a href='http://freshmeat.net/projects/phplicensewatcher/'>PhpLicensewatcher</a> $version_id\n");
+echo("<p>Page last refreshed at: " . DATE("Y-m-d H:i:s") . "\n");
 
 $End = getTime();
 echo "<br>Execution time: ".number_format(($End - $Start),2)."s";
